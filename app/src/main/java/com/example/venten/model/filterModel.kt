d2c0378296608fd5c957698cb47fdf8e8de28a68
Goodnew <mikeblize@gmail.com>
2020-08-27 10:47:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/venten/model/filterModel.kt
@@ -0,0 +1,10 @@
+package com.example.venten.model
+//data class FilterModel (
+//
+////    @SerializedName("id") val id : Int,
+////    @SerializedName("start_year") val start_year : Int,
+////    @SerializedName("end_year") val end_year : Int,
+////    @SerializedName("gender") val gender : String,
+////    @SerializedName("countries") val countries : List<String>,
+////    @SerializedName("colors") val colors : List<String>
+//)
